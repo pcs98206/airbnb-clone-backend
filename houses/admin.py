b23dfb2bf60151e63348307c07b25b7ceba4fb1d
한상규 <hansang-gyu@hansang-gyuui-MacBookAir.local>
2023-03-11 09:34:18 +0900

    Documentation

--- houses/admin.py
+++ houses/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import House
 
 # Register your models here.
+
+
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    fields = ("name", "address", "description", ("price_per_night", "pets_allowed"))
+    list_display = ("name", "price_per_night", "description", "address", "pets_allowed")
+    list_filter = ("price_per_night", "pets_allowed")
+    search_fields = ("address",)
+    list_display_links = ("name", "address")
+    list_editable = ("pets_allowed",)

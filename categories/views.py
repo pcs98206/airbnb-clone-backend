9cf56cbc638423679214c7986be6c4b280218ea0
한상규 <hansang-gyu@hansang-gyuui-MacBookAir.local>
2023-04-21 20:42:58 +0900

    api_view

--- categories/views.py
+++ categories/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
 # Create your views here.
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "ok": True,
-            "categories": all_categories,
-        },
+            "categories": Category.objects.all(),
+        }
     )

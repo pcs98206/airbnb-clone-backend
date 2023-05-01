7ce6fb5edec876c92bf37d0596e87d36a4fa165f
한상규 <hansang-gyu@hansang-gyuui-MacBookAir.local>
2023-05-01 12:37:21 +0900

    update()

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -20,8 +21,20 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_data = serializer.save()
+            return Response(CategorySerializer(updated_data).data)
+        else:
+            return Response(serializer.errors)
